b4cb8a668ab8c00f2fd6db6ef4e7a6e0c6d22e87
sybil <jordan.sybil@hotmail.com>
2024-04-04 20:42:42 -0700

    test2

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -6,11 +6,13 @@ class KeyValuePair {
   }
 }
 
-class HashTable { // get O(1), set O(1), deleteKey O(1)
+class HashTable {
+  // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -28,27 +30,45 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     return this.hash(key) % this.capacity;
   }
 
-
   insert(key, value) {
-    // Your code here
-  }
+    const index = this.hashMod(key);
 
+    let curr = this.data[index];
+    while (curr && curr.key !== key) {
+      curr = curr.next;
+    }
+
+    if (curr) {
+      curr.value = value;
+    } else {
+      const newPair = new KeyValuePair(key, value);
+
+      if (!this.data[index]) {
+        this.data[index] = newPair;
+      } else {
+        newPair.next = this.data[index];
+        this.data[index] = newPair;
+      }
+      this.count++;
+    }
+  }
 
   read(key) {
-    // Your code here
+  if(this.data[index] === new KeyValuePair(key)){
+    return this.data[index]
+  }else{
+   return  undefined
   }
-
+  }
 
   resize() {
     // Your code here
   }
 
-
   delete(key) {
     // Your code here
   }
 }
-
 
 module.exports = HashTable;
 //test
