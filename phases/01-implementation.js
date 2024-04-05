f6f268a8277a62fcf8b8a632eb7f6044297a10b7
atxmikaela <mikaela@mikaela@mikaelataylor.com>
2024-04-05 00:21:27 -0500

    partially complete

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -54,21 +54,45 @@ class HashTable {
   }
 
   read(key) {
-  if(this.data[index] === new KeyValuePair(key)){
-    return this.data[index]
-  }else{
-   return  undefined
-  }
+
+      const index = this.hashMod(key);
+
+      let curr = this.data[index];
+      while (curr){
+      if (curr.key === key){
+        return curr.value;
+      }
+      curr = curr.next
+      }
+      return undefined;
+
   }
 
   resize() {
     // Your code here
-  }
+    const oldData = this.data;
+
+    this.count = 0;
+    this.capacity = this.capacity * 2;
+    this.data = new Array(this.capacity).fill(null);
+
+    let currentNode = null;
+
+    for (let i = 0; i < oldData.length; i++) {
+      currentNode = oldData[i];
+      while (currentNode) {
+        this.insert(currentNode.key, currentNode.value)}
+        currentNode = currentNode.next;
+      }
+    }
+
+  
 
   delete(key) {
     // Your code here
   }
 }
+
 
 module.exports = HashTable;
 //test
